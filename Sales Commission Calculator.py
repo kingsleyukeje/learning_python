c2b1b05bbad1bc04776aca2ad11b336914118d22
Kingsley Ukeje <30627493+kingsleyukeje@users.noreply.github.com>
2023-07-27 17:46:35 +0100

    Delete Sales Commission Calculator.py

--- Sales Commission Calculator.py
+++
@@ -1,7 +0,0 @@
-earnings = float(input("Enter your earning for the week? "))
-calc = (9/100) * earnings
-
-searnings = calc + 200
- 
-print ("Your total sales persons earning for the week is: " + str(searnings))
-
