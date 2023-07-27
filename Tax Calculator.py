2172a15b42f94525389ce6008f1a6b1a36e52c6f
Kingsley Ukeje <30627493+kingsleyukeje@users.noreply.github.com>
2023-07-27 17:46:46 +0100

    Delete Tax Calculator.py

--- Tax Calculator.py
+++
@@ -1,8 +0,0 @@
-cname = input("What is your name? ")
-earnings = int(input("what are your earning? "))
-if earnings <= 30000:
-    calc = (15/100) * earnings
-    print("Your tax is " + str(calc))
-else:
-    calcc = (20/100) * earnings
-    print("Your tax is " + str(calcc))
